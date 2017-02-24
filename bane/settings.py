b54b5a6edcf8222e6a6d7bea4a035f35d289e994
KJ <kj1352>
2017-02-25 02:21:10 +0530

    Allowed Hosts

--- bane/settings.py
+++ bane/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0)zemth9ap0sbyfdyyu*_d%wkc7!r*^by59tczqd26g_+ok_v('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bane-was-bored.herokuapp.com']
 
 
 # Application definition
